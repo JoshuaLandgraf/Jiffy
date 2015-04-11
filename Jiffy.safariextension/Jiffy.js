e339d075af4f53b0d3260574e3adcd64aa9f5bc0
Joshua Landgraf <joshua@Joshs-MacBook-Pro.local>
2015-04-11 08:26:56 -0700

    Initial Release

---
+++ Jiffy.safariextension/Jiffy.js
@@ -0,0 +1,49 @@
+// Much thanks to http://stackoverflow.com/questions/179713/how-to-change-the-href-for-a-hyperlink-using-jquery/28016553#28016553 and http://www.w3schools.com/ajax/ajax_xmlhttprequest_send.asp
+
+function GIFtoGIFV(source) {
+	/*var links = source.querySelectorAll('img[src$=".gif"]');
+	Array.prototype.forEach.call(links, function(element, index) {
+		var request = new XMLHttpRequest();
+		request.onreadystatechange = function() {
+			if(request.readyState == 4 && request.status == 200) {
+				console.log(element.src);
+				element.src = element.src + "v";
+				console.log("GIF was updated to " + element.src);
+			} else if (request.readyState == 4 && request.status != 200) {
+				console.log(element.src + "v is not a valid URL, not updating GIF");
+			}
+		}
+		request.open("GET", element.src + "v", true);
+		request.send();
+	});*/
+
+	var links = source.querySelectorAll('a[href$=".gif"]');
+	Array.prototype.forEach.call(links, function(element, index) {
+		var request = new XMLHttpRequest();
+		request.onreadystatechange = function() {
+			if(request.readyState == 4 && request.status == 200) {
+				element.href = element.href + "v";
+				console.log("updated \"" + element.innerHTML + "\" to link to " + element.href);
+			} else if (request.readyState == 4 && request.status != 200) {
+				console.log(element.href + "v is not a valid URL, not updating link");
+			}
+		}
+		request.open("GET", element.href + "v", true);
+		request.send();
+	});
+}
+
+var observer = new MutationObserver(function(records) {
+	records.forEach(function(record) {
+		if (!record.target) GIFtoGIFV(record.target);
+		/*if (record.addedNodes.length == 0) return;
+		Array.prototype.forEach.call(record.addedNodes, function (element, index) {
+			GIFtoGIFV(element);
+		});*/
+		
+	})
+});
+
+observer.observe(document, {childList: true, attributes: true, characterData: true,  subtree: true});
+
+GIFtoGIFV(document)
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Jiffy
 Browser Extension that replaces links to GIFs with links to GIFVs
+
+## Inspiration
+HTML5 GIFs (GIFVs) load faster and play better than the original GIF format. We wanted to make a browser extension that uses GIFVs where possible.
+
+## How it works
+Our extension finds links to GIFs on your current webpages and checks if a GIFV with a similar link exists. If so, it will automatically update the original link to the better GIFV file.
+
+## Browser support
+Our extension currently supports both Chrome and Safari
